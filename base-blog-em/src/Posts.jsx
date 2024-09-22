f5f4ed5aee7d8a9845e62b61bed0a026f56dec9e
NT750XGR-A31AS <goo.gl.denver@gmail.com>
2024-09-22 16:59:21 +0900

    섹션 2: 페이지 매김, 프리패칭과 변이
    12. 페이지 매김(Pagination)

---
+++ base-blog-em/src/Posts.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchPosts, deletePost, updatePost } from "./api";
+import { PostDetail } from "./PostDetail";
+const maxPostPage = 10;
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 1000,
+  });
+
+  if (isLoading) {
+    return <em>읽어들이는 중...</em>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <em>Oops!</em>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
